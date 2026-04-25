9b7f561ca2abeda841f758f9a2469feaceea0b08
kamay <admin@xutaxkamay.com>
2026-04-25 20:05:05 +0200

    Added daScript master repo

--- daslang/include/daScript/misc/debug_break.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void DAS_API os_debug_break();
--- daslang/include/daScript/simulate/aot_builtin_dasbind.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "daScript/misc/platform.h"
-
-namespace das {
-    DAS_API char * safe_pass_string ( const char * str );
-}
